dbf5d8d17411713e7ace51b8b81e7ba4e1d08b8c
Gary Tou <gary@garytou.com>
2021-11-27 02:53:05 -0800

    Attempt _next rewrite

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
 	reactStrictMode: true,
 	images: {
 		domains: ['garytou.com', 'assets.garytou.com', '*.garytou.com'],
 	},
+	assetPrefix: isProd ? '/home' : '',
+	rewrites: [
+		{
+			source: '/home/_next/:path*',
+			destination: '/_next/:path*',
+		},
+	],
 };
